b39da27847e25c0b60c8f2e8fe0623efe0e6f143
silentJob <silent@jetfuel.com.tw>
2018-03-26 16:13:08 +0800

    finish publish article and that view

--- src/components/App.vue
+++ src/components/App.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="container-fluid">
   <nav class="navbar navbar-expand-lg navbar-light bg-light">
-    <a class="navbar-brand" href="#">{{username}}</a>
+    <span class="navbar-brand">{{username}}</span>
     <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
       <span class="navbar-toggler-icon"></span>
     </button>
@@ -9,7 +9,7 @@
     <div class="collapse navbar-collapse" id="navbarSupportedContent">
       <ul class="navbar-nav mr-auto">
         <li class="nav-item active">
-          <a class="nav-link" href="#">Home <span class="sr-only">(current)</span></a>
+          <router-link class="nav-link" :to="{path: '/'}" >Home<span class="sr-only">(current)</span></router-link>
         </li>
 
         <li class="nav-item dropdown">
@@ -29,9 +29,9 @@
       </ul>
 
     </div>
-    <router-link v-if='isLogin' :to="{path: 'article_create'}" >test</router-link>
-    <router-link v-if='isLogin' :to="{name: 'login'}" >登出</router-link>
-    <router-link v-else :to="{name: 'login'}" >登入</router-link>
+    <router-link v-if='isLogin' class="nav-link" :to="{path: 'article_create'}" >發表文章</router-link>
+    <router-link v-if='isLogin' class="nav-link" :to="{name: 'login'}" >登出</router-link>
+    <router-link v-else class="nav-link" :to="{name: 'login'}" >登入</router-link>
   </nav>
 
     <div class="col-md-12">
@@ -76,7 +76,7 @@
                 'Content-Type': 'application/json'
               }
             })
-            // todo 已經可以確認登入了
+            // todo 已經可以確認登入了 type轉換
             return instance.get('http://localhost/api/user/')
               .then(function (response) {
                 if (response.data.code == 200) {
--- src/components/ArticleCreate.vue
+++ src/components/ArticleCreate.vue
@@ -1,11 +1,71 @@
 <template>
-    <h1>12345</h1>
+  <div class="row">
+    <div class="col-xs-12 col-md-10 col-lg-6">
+      <form v-on:submit.prevent="publish()" class="panel panel-default" id="publish">
+        <div class="panel-body">
+          <fieldset>
+            <legend class="section">發表文章</legend>
+            <div class="form-group">
+              <label for="type" class="control-label">type</label>
+              <select class="form-control" id="type" name="type">
+                <option value="" selected="selected">--请选择--</option>
+                <option value="0">公告</option>
+                <option value="1">討論</option>
+                <option value="2">問題</option>
+              </select>
+            </div>
+            <div class="form-group">
+              <label for="title" class="control-label">Title</label>
+              <input class="form-control" name="title" type="text" id="title">
+            </div>
+            <div class="form-group">
+              <label for="note" class="control-label">內容</label>
+              <input class="form-control" name="content" type="text" id="note">
+            </div>
+          </fieldset>
+        </div>
+        <div class="panel-footer clearfix">
+          <div class="btns pull-right">
+            <button class="btn btn-success">發表</button>
+          </div>
+        </div>
+      </form>
+    </div>
+  </div>
 </template>
 
 <script>
-    export default {
-        name: "create-article"
+  export default {
+    name: "create-article",
+    methods: {
+      publish() {
+        let publish = new FormData(document.getElementById('publish'));
+        let jwt = this.$cookie.get('jwtToken');
+        var self = this;
+        var instance = axios.create({
+          headers: {
+            'Authorization': 'Bearer ' + jwt,
+            'Accept': 'application/json',
+            'Content-Type': 'application/json'
+          }
+        })
+        instance.post('http://localhost/api/article', publish)
+          .then(function (response) {
+            if (response.data.code == 200) {
+              //todo 沒有提示成功跟失敗，type有權限的問題
+              self.$router.push({name:'index'});
+            }
+            else
+              {
+                console.log(response.data.code);
+              }
+          })
+          .catch(function (error) {
+            console.log(error)
+          })
+      }
     }
+  }
 </script>
 
 <style scoped>
